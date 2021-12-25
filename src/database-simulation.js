721dfb33e2e4d27dc657389a16da719a531672f3
ZiF1R <do-alex03@mail.ru>
2021-12-25 19:30:58 +0300

    Change structure of pizza table in database-simulation.js

--- src/database-simulation.js
+++ src/database-simulation.js
@@ -6,29 +6,124 @@ const MIN_COEFF = 1,
   MIDDLE_COEFF = 1.2,
   BIG_COEFF = 1.5;
 
+const TRADITIONAL_BORDER = "традиционная",
+  THIN_BORDER = "тонкая";
+
 const pizzaTable = [
   {
     name: "Чизбургер-пицца",
     icon: "",
-    sizes: [MINSIZE, MIDDLESIZE, BIGSIZE],
+    sizes: [
+      {
+        value: MINSIZE,
+        inStock: true,
+      },
+      {
+        value: MIDDLESIZE,
+        inStock: true,
+      },
+      {
+        value: BIGSIZE,
+        inStock: true,
+      },
+    ],
+    borders: [
+      {
+        value: THIN_BORDER,
+        inStock: true,
+      },
+      {
+        value: TRADITIONAL_BORDER,
+        inStock: false,
+      },
+    ],
     initialCost: 395,
   },
   {
     name: "Сырная",
     icon: "",
-    sizes: [MINSIZE],
+    sizes: [
+      {
+        value: MINSIZE,
+        inStock: true,
+      },
+      {
+        value: MIDDLESIZE,
+        inStock: false,
+      },
+      {
+        value: BIGSIZE,
+        inStock: false,
+      },
+    ],
+    borders: [
+      {
+        value: THIN_BORDER,
+        inStock: true,
+      },
+      {
+        value: TRADITIONAL_BORDER,
+        inStock: true,
+      },
+    ],
     initialCost: 450,
   },
   {
     name: "Креветки по-азиатски",
     icon: "",
-    sizes: [MIDDLESIZE],
+    sizes: [
+      {
+        value: MINSIZE,
+        inStock: false,
+      },
+      {
+        value: MIDDLESIZE,
+        inStock: true,
+      },
+      {
+        value: BIGSIZE,
+        inStock: false,
+      },
+    ],
+    borders: [
+      {
+        value: THIN_BORDER,
+        inStock: true,
+      },
+      {
+        value: TRADITIONAL_BORDER,
+        inStock: true,
+      },
+    ],
     initialCost: 230,
   },
   {
     name: "Сырный цыпленок",
     icon: "",
-    sizes: [MIDDLESIZE, BIGSIZE],
+    sizes: [
+      {
+        value: MINSIZE,
+        inStock: false,
+      },
+      {
+        value: MIDDLESIZE,
+        inStock: true,
+      },
+      {
+        value: BIGSIZE,
+        inStock: true,
+      },
+    ],
+    borders: [
+      {
+        value: THIN_BORDER,
+        inStock: false,
+      },
+      {
+        value: TRADITIONAL_BORDER,
+        inStock: true,
+      },
+    ],
     initialCost: 275,
   },
 ];
