5de848407ecbbeeee8b4332087cc6539b61a73b3
ZiF1R <do-alex03@mail.ru>
2021-12-26 19:30:42 +0300

    Change structure of database-simulation.js

--- src/database-simulation.js
+++ src/database-simulation.js
@@ -1,3 +1,4 @@
+// sizes constants
 const MINSIZE = 26,
   MIDDLESIZE = 30,
   BIGSIZE = 40;
@@ -6,8 +7,12 @@ const MIN_COEFF = 1,
   MIDDLE_COEFF = 1.2,
   BIG_COEFF = 1.5;
 
+// border constants
 const TRADITIONAL_BORDER = "традиционная",
   THIN_BORDER = "тонкая";
+
+const THIN_BORDER_ADDITION = 0,
+  TRADITIONAL_BORDER_ADDITION = 30;
 
 const pizzaTable = [
   {
@@ -17,24 +22,29 @@ const pizzaTable = [
       {
         value: MINSIZE,
         inStock: true,
+        coeff: MIN_COEFF,
       },
       {
         value: MIDDLESIZE,
         inStock: true,
+        coeff: MIDDLE_COEFF,
       },
       {
         value: BIGSIZE,
         inStock: true,
+        coeff: BIG_COEFF,
       },
     ],
     borders: [
       {
         value: THIN_BORDER,
         inStock: true,
+        addition: 0,
       },
       {
         value: TRADITIONAL_BORDER,
-        inStock: false,
+        inStock: true,
+        addition: 30,
       },
     ],
     initialCost: 395,
@@ -46,24 +56,29 @@ const pizzaTable = [
       {
         value: MINSIZE,
         inStock: true,
+        coeff: MIN_COEFF,
       },
       {
         value: MIDDLESIZE,
         inStock: false,
+        coeff: MIDDLE_COEFF,
       },
       {
         value: BIGSIZE,
-        inStock: false,
+        inStock: true,
+        coeff: BIG_COEFF,
       },
     ],
     borders: [
       {
         value: THIN_BORDER,
         inStock: true,
+        addition: THIN_BORDER_ADDITION,
       },
       {
         value: TRADITIONAL_BORDER,
         inStock: true,
+        addition: TRADITIONAL_BORDER_ADDITION,
       },
     ],
     initialCost: 450,
@@ -75,24 +90,29 @@ const pizzaTable = [
       {
         value: MINSIZE,
         inStock: false,
+        coeff: MIN_COEFF,
       },
       {
         value: MIDDLESIZE,
         inStock: true,
+        coeff: MIDDLE_COEFF,
       },
       {
         value: BIGSIZE,
         inStock: false,
+        coeff: BIG_COEFF,
       },
     ],
     borders: [
       {
         value: THIN_BORDER,
         inStock: true,
+        addition: THIN_BORDER_ADDITION,
       },
       {
         value: TRADITIONAL_BORDER,
         inStock: true,
+        addition: TRADITIONAL_BORDER_ADDITION,
       },
     ],
     initialCost: 230,
@@ -104,36 +124,33 @@ const pizzaTable = [
       {
         value: MINSIZE,
         inStock: false,
+        coeff: MIN_COEFF,
       },
       {
         value: MIDDLESIZE,
         inStock: true,
+        coeff: MIDDLE_COEFF,
       },
       {
         value: BIGSIZE,
         inStock: true,
+        coeff: BIG_COEFF,
       },
     ],
     borders: [
       {
         value: THIN_BORDER,
         inStock: false,
+        addition: THIN_BORDER_ADDITION,
       },
       {
         value: TRADITIONAL_BORDER,
         inStock: true,
+        addition: TRADITIONAL_BORDER_ADDITION,
       },
     ],
     initialCost: 275,
   },
 ];
-
-// calculate prices for all pizza for all sizes
-for (let p of pizzaTable)
-  p["prices"] = [
-    ~~(p.initialCost * MIN_COEFF),
-    ~~(p.initialCost * MIDDLE_COEFF),
-    ~~(p.initialCost * BIG_COEFF),
-  ];
 
 export { pizzaTable };
